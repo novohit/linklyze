2585adde1a2cf83c98790e693da0d50e5505148e
novo <weixiong_zhu@163.com>
2023-02-20 17:10:08 +0800

    feat: first commit

---
+++ plato-account/src/main/java/com/wyu/account/AccountApplication.java
@@ -0,0 +1,15 @@
+package com.wyu.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author novo
+ * @date 2023-02-20 16:48
+ */
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
