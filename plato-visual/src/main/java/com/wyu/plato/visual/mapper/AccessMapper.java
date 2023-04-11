0c3f831135709e1558cb646ccedda39b04c2497c
novo <zwx_info@163.com>
2023-04-11 18:21:34 +0800

    update: 限制分页实时查询访问记录最大条数

--- plato-visual/src/main/java/com/wyu/plato/visual/mapper/AccessMapper.java
+++ plato-visual/src/main/java/com/wyu/plato/visual/mapper/AccessMapper.java
@@ -15,9 +15,14 @@ public interface AccessMapper extends BaseMapper<DwsWideInfo> {
     List<RegionStatsVO> region(@Param("code") String code, @Param("start") String start, @Param("end") String end);
 
     List<TypeGroupByDO> type(@Param("code") String code, @Param("start") String start, @Param("end") String end);
+
     List<TrendGroupByDO> trendByDay(@Param("code") String code, @Param("start") String start, @Param("end") String end);
 
     List<TrendGroupByDO> trendByHour(@Param("code") String code, @Param("date") String date);
 
     List<RefererGroupByDO> refererTopN(@Param("code") String code, @Param("start") String start, @Param("end") String end, @Param("n") Integer n);
+
+    List<DwsWideInfo> pageRecord(@Param("code") String code, @Param("offset") int offset, @Param("size") int size);
+
+    int pagRecordTotal(@Param("code") String code, @Param("maxLimit") Integer maxLimit);
 }
