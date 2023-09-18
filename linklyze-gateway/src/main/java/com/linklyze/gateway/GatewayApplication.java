f36e7e19dcc50be5a3481cfc6fbbffaed0d8625e
novo <zwx_info@163.com>
2023-09-18 14:17:38 +0800

    chore: remove unnecessary code

--- linklyze-gateway/src/main/java/com/linklyze/gateway/GatewayApplication.java
+++ linklyze-gateway/src/main/java/com/linklyze/gateway/GatewayApplication.java
@@ -3,14 +3,12 @@ package com.linklyze.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author novo
  * @since 2023-02-22 16:54
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.linklyze.account", "com.linklyze.gateway"})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
