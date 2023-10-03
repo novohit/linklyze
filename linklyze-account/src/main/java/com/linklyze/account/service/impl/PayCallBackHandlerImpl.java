1ae2d6fb7e6c6c7cf37bc5efeef6a2b294846d91
novohit <zwx_info@163.com>
2023-10-03 21:38:13 +0800

    refactor: 支付回调多次推送避免 MQ 发送重复消息

--- linklyze-account/src/main/java/com/linklyze/account/service/impl/PayCallBackHandlerImpl.java
+++ linklyze-account/src/main/java/com/linklyze/account/service/impl/PayCallBackHandlerImpl.java
@@ -1,23 +1,21 @@
 package com.linklyze.account.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.linklyze.account.config.RabbitMQConfig;
 import com.linklyze.account.mapper.ProductOrderMapper;
-import com.linklyze.account.model.ProductOrderDO;
 import com.linklyze.account.service.strategy.PayCallBackResponse;
 import com.linklyze.account.service.strategy.PayCallbackHandler;
 import com.linklyze.common.enums.MessageEventType;
-import com.linklyze.common.enums.PayStateEnum;
 import com.linklyze.common.enums.PayType;
 import com.linklyze.common.model.bo.CustomMessage;
 import com.linklyze.common.util.JsonUtil;
 import com.linklyze.common.util.uuid.IDUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -27,9 +25,12 @@ public class PayCallBackHandlerImpl implements PayCallbackHandler {
 
     private final RabbitTemplate rabbitTemplate;
 
-    public PayCallBackHandlerImpl(ProductOrderMapper productOrderMapper, RabbitTemplate rabbitTemplate) {
+    private final StringRedisTemplate redisTemplate;
+
+    public PayCallBackHandlerImpl(ProductOrderMapper productOrderMapper, RabbitTemplate rabbitTemplate, StringRedisTemplate redisTemplate) {
         this.productOrderMapper = productOrderMapper;
         this.rabbitTemplate = rabbitTemplate;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -45,16 +46,29 @@ public class PayCallBackHandlerImpl implements PayCallbackHandler {
                 .messageId(IDUtil.fastUUID())
                 .eventType(MessageEventType.ORDER_PAY_SUCCESS)
                 .build();
-        // 异步修改订单状态和下发流量包
-        log.info("向MQ发送消息,message:[{}]", message);
-        this.rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_EVENT_EXCHANGE, RabbitMQConfig.ORDER_PAY_SUCCESS_ROUTING_KEY, message);
-        log.info(
-                "订单支付成功，订单号：{}，支付方式：{}，订单详情：{}，订单金额：{}",
-                outTradeNo,
-                PayType.ALI_PAY_PC,
-                subject,
-                totalAmount
-        );
-        return new PayCallBackResponse("success");
+        // 上游第三方支付回调消息不是幂等的，有可能发送多次消息，需要下游在业务或数据库上保证幂等性
+        Boolean success = redisTemplate.opsForValue().setIfAbsent(outTradeNo, "SUCCESS", 12, TimeUnit.HOURS);
+        if (Boolean.TRUE.equals(success)) {
+            // 异步修改订单状态和下发流量包
+            log.info("向MQ发送消息,message:[{}]", message);
+            this.rabbitTemplate.convertAndSend(RabbitMQConfig.ORDER_EVENT_EXCHANGE, RabbitMQConfig.ORDER_PAY_SUCCESS_ROUTING_KEY, message);
+            log.info(
+                    "订单支付成功，订单号：{}，支付方式：{}，订单详情：{}，订单金额：{}",
+                    outTradeNo,
+                    PayType.ALI_PAY_PC,
+                    subject,
+                    totalAmount
+            );
+            return new PayCallBackResponse("success");
+        } else {
+            log.error(
+                    "订单已处理，订单号：{}，支付方式：{}，订单详情：{}，订单金额：{}",
+                    outTradeNo,
+                    PayType.ALI_PAY_PC,
+                    subject,
+                    totalAmount
+            );
+            return new PayCallBackResponse("fail");
+        }
     }
 }
